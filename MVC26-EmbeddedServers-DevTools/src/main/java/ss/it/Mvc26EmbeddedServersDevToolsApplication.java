40615c239d0461d6a71186a22c2adb70dce2611b
suresh <suresh@DESKTOP-81HU9S4>
2022-04-29 15:15:46 +0530

    devtools and embedded servers

---
+++ MVC26-EmbeddedServers-DevTools/src/main/java/ss/it/Mvc26EmbeddedServersDevToolsApplication.java
@@ -0,0 +1,13 @@
+package ss.it;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mvc26EmbeddedServersDevToolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mvc26EmbeddedServersDevToolsApplication.class, args);
+	}
+
+}
